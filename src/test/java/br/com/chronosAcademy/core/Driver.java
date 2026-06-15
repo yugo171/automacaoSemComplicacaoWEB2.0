b99e0c2ed8ea51033d8ff0be1117a37241517e42
yugo171 <contadepc321@gmail.com>
2026-06-14 23:59:27 -0300

    aulas sobre POM e refactory

---
+++ src/test/java/br/com/chronosAcademy/core/Driver.java
@@ -0,0 +1,44 @@
+package br.com.chronosAcademy.core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Driver { //objeto precisa de proteção então ele vem como private
+
+    private WebDriver driver;
+
+    public Driver(String navegador){
+        switch (navegador){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new         ChromeDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+    }
+
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
